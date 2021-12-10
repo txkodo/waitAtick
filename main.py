ebc416ab6f36e5f72f479c0886c1b2475b8c52af
txkodo <kodo.hsmkai@gmail.com>
2021-12-10 11:00:36 +0900

    pythonのコメントを修正

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from waitatick import waitAtickDynamic, waitAtickStatic
 
-# デフォルトのmainスケジュールの作成
+### dynamic_exスケジュールの作成
+# スケジュール名、待機後に実行する関数のリストを引数に取る
 waitAtickDynamic('dynamic_ex',["example:dynamic_callback"])
 
+### static_exスケジュールの作成
+# スケジュール名、待機時間(チック)、待機後に実行する関数のリストを引数に取る
 waitAtickStatic('static_ex',10,["example:static_callback"])
